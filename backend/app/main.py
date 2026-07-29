fc403122fa9eeb76ee6bb751b7416cef29bc0092
futsu <futsu037@gmail.com>
2026-07-29 19:36:24 +0300

    Connect frontend with backend via docker.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI
 from app.api import user, authentication, friendship, post, comment, like, profile
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+      "http://localhost:5173",
+
+      "http://localhost:3000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(user.router)
 app.include_router(authentication.router)
@@ -9,6 +22,7 @@ app.include_router(post.router)
 app.include_router(comment.router)
 app.include_router(like.router)
 app.include_router(profile.router)
+
 
 @app.get("/")
 def root():
