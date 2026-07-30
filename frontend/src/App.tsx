e373e5a4d683f9b40467509e442b54d67297aaf5
futsu <futsu037@gmail.com>
2026-07-30 15:30:09 +0300

    Remove SignUp.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,8 @@
-import { useState, useEffect } from "react";
 import "./App.css";
-
-type ApiResponse = {
-  message: string;
-};  
-
 function App() {
-  const [data, setData] = useState<ApiResponse | null>(null);
-
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const response = await fetch("http://localhost:8000/");
-        const result = await response.json();
-
-        setData(result);
-      } catch (err) {
-        console.error(err);
-      }
-    }
-
-    fetchData();
-  }, []);
 
   return (
-    <div className="App">
-      <h1>Hello, World!</h1>
-      <p>{data?.message}</p>
-    </div>
+    <></>
   );
 }
 
